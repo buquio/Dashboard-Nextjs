d58440c17719aa2cde156a9ff7fceaeb1f54e387
kehinde bukola <kehinde.bukola14@gmail.com>
2024-01-30 19:17:22 +0000

    initial commit

---
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+    return <p>Dashboard Page</p>;
+  }
+// dashboard page  http://localhost:3001/dashboard  
+// Customers Page: The page should be accessible on http://localhost:3000/dashboard/customers. For now, it should return a <p>Customers Page</p> element.
+// Invoices Page: The invoices page should be accessible on http://localhost:3000/dashboard/invoices. For now, also return a <p>Invoices Page</p> element.
